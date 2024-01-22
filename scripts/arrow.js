0d2abf0dcb47d3de152511cd1e735a3bf177270b
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2024-01-22 14:29:26 -0500

    arrow

---
+++ scripts/arrow.js
@@ -0,0 +1,10 @@
+const person = {
+ firstName: 'Grace',
+ lastName: 'Hopper',
+ age: 89,
+ fullName: () => { return this.firstName + ' ' + this.lastName }
+}
+
+let x = person.fullName();
+console.log(x);
+ 
