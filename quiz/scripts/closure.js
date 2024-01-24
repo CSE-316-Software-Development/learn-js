cda5580e829ef883b5fdce08f607ca1bf4837b70
deshpandeadi1901 <deshpande.aditya@northeastern.edu>
2024-01-24 16:26:25 -0500

    in class work

--- quiz/scripts/closure.js
+++ quiz/scripts/closure.js
@@ -1,10 +1,11 @@
 function setColor(set) {
         let changeColor = set;
-        if(changeColor) {
-            let userColor = document.getElementById('color').value;
-            document.getElementById('myPara').style.color = userColor;
+        return () => {
+            if(changeColor) {
+                let userColor = document.getElementById('color').value;
+                document.getElementById('myPara').style.color = userColor;
+            }
         }
-        
 }
 
 window.onload = function() {
--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,8 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg() {
+    console.log(`Error: ${this.errMsg}`);
+}
+const obj1 = {
+    errMsg: "error has occured"
+}
+logMsg.call(obj1);
