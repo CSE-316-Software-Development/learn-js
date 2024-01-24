a0208a7c6ee5cb695339801030fe965b0f2f40bf
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2024-01-24 14:28:32 -0500

    minor typo

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,18 +1,1 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
-
-function logMsg() {
-    console.log(`Error Message: ${this.errMsg}`);
-}
-
-const o1 = {
-    val: [1,2,3],
-    errMsg: 'Error from o1'
-};
-
-const o2 = {
-    val: 'hello o2',
-    errMsg: 'Error from o2'
-};
-
-logMsg.call(o1);
-logMsg.call(o2);
