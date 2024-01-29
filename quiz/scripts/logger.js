1763b644f608cb235e88e785c69360488ba89897
mmehdiali5 <mmehdiali5@gmail.com>
2024-01-28 23:27:04 -0500

    logger

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,20 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg() {
+    if (this && this.errMsg) {
+        console.error(this.errMsg);
+    } else {
+        console.log("No error message found.");
+    }
+}
+
+
+const objectWithErrorMessage = {
+    errMsg: "This is an error message."
+};
+
+const objectWithoutErrorMessage = {
+    someData: "No error message here."
+};
+
+logMsg.call(objectWithErrorMessage);     // Logs the error message
+logMsg.call(objectWithoutErrorMessage);
