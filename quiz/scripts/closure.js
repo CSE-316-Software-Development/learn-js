8c0fb9ac752dcc50d734b5db3e1481d739697fe7
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-01-30 12:57:35 -0500

    fixed closure.js script

--- quiz/scripts/closure.js
+++ quiz/scripts/closure.js
@@ -1,9 +1,11 @@
 function setColor(set) {
         let changeColor = set;
-        if(changeColor) {
+        return () => { // added (KS), need to return a closure!
+            if(changeColor) {
             let userColor = document.getElementById('color').value;
             document.getElementById('myPara').style.color = userColor;
         }
+    }
         
 }
 
@@ -11,3 +13,8 @@ window.onload = function() {
     let toggle = true;
     document.getElementById('btn').onclick = setColor(toggle);
 }
+
+
+// error: the function setColor is not actually returning anything!
+// fix: need the function to execute when the click happens. To do this, we need to create a nested function, aka a closure!!
+// added line 3 to add the arrow func
