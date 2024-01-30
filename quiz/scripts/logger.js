8f0590ce4f4c4caf14c29a6fc39b03ccd8767ff6
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-01-30 12:45:05 -0500

    fixed error in logger.js

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,11 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+
+function logMsg() {
+    console.log(`Error message: ${this.errMsg}`);
+};
+
+const o1 = {
+    errMsg: '01'
+};
+
+logMsg.call(o1)
