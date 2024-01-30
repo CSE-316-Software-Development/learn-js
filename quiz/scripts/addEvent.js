2c59f47bc6aa07685ef79d9ab9d2d972b4867de8
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-01-30 13:19:27 -0500

    fixed errors in addEvents.js

--- quiz/scripts/addEvent.js
+++ quiz/scripts/addEvent.js
@@ -1,18 +1,24 @@
 window.onload = function() {
     let x = document.getElementById('myBtn');
-    x.addEventListener('mouseover', myFunction('Moused over!'));
+    x.addEventListener('mouseover', myFunction('Moused over!')); // now these functions take input parameters!
     x.addEventListener('click', mySecondFunction('Clicked!'));
     x.addEventListener('mouseout', myThirdFunction('Moused out!'));
 }
 
 function myFunction(msg) {
-    document.getElementById('demo').textContent = msg;
+    return () => document.getElementById('demo').textContent = msg;
 }
 
 function mySecondFunction(msg) {
-    document.getElementById('demo').textContent = msg;
+    return () => document.getElementById('demo').textContent = msg;
 }
 
 function myThirdFunction(msg) {
-    document.getElementById('demo').textContent = msg;
+    return () => document.getElementById('demo').textContent = msg;
 }
+
+// why doesnt this work as expected? What changed now that the functions in EventListeners take parameters?
+// maybe that the functions to be handled don't return anything? 
+// the functions need to return CLOSURES! 
+// How do we do this? Convert the function into arrow function at line 3
+// OR return closures below in the individual functions! (done KS)
