637195d0a3f7d1c6aa2182526f64d921f628974d
Chen <chen@ChendeMacBook-Air.local>
2024-01-30 16:50:50 -0500

    class ac

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,4 +1,13 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
-function logMsg(object){
-    console.log(object.errMsg);
+
+const logger = {
+    logMsg: function (){
+        console.log(this.errMsg)
+    }
 }
+
+const obj1 = {
+    errMsg: "Obj1"
+}
+
+logger.logMsg.call(obj1)
