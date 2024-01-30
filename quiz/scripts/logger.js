43af5ab9f57c126c98ef903416ec747fa7474c29
Chen <chen@ChendeMacBook-Air.local>
2024-01-30 15:29:09 -0500

    starter 013024

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,4 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg(object){
+    console.log(object.errMsg);
+}
