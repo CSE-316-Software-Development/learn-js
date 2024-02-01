c120fe44deb43dcb0a347e3448c798df5d8e57d0
Duolikun-Daniya <daniya.d@northeastern.edu>
2024-02-01 16:42:14 -0500

    Complete Manipulating The DOM

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -1,12 +1,12 @@
-window.onload = function() {
-  document.getElementById('addTableBtn').addEventListener('click', addTable);
-}
+window.onload = function () {
+  document.getElementById("addTableBtn").addEventListener("click", addTable);
+};
 
 function createTRNode(colNodes) {
   let trNode = document.createElement("tr");
-  colNodes.forEach(function(colNode) {
+  colNodes.forEach(function (colNode) {
     trNode.appendChild(colNode);
-  })
+  });
   return trNode;
 }
 
@@ -21,11 +21,31 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function createBtnNode(btnTxt, e, l) {
+  let btnNode = document.createElement("button");
+  let btnNodeText = createTxtNode(btnTxt);
+  btnNode.appendChild(btnNodeText);
+  btnNode.addEventListener(e, l);
+  return btnNode;
+}
+
+function edit(node) {
+  let newNode = document.createElement("input");
+  newNode.type = "text";
+  newNode.placeholder = "Enter Cell (x, y) ...";
+  node.replaceChild(newNode, node.childNodes[0]);
+}
+
 function addTable() {
   const tableNode = document.createElement("table");
-  for(let i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col2 = createTDNode(
+      createBtnNode("Edit text", "click", function () {
+        edit(col1);
+      })
+    );
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
