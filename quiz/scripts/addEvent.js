379fee14c3b0f1b3f9159665c23a492c06fd084b
Zhan Su <su.zhan@northeastern.edu>
2024-02-03 22:12:09 -0500

    updated 013124 lecture content

--- quiz/scripts/addEvent.js
+++ quiz/scripts/addEvent.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     let x = document.getElementById('myBtn');
-    x.addEventListener('mouseover', myFunction('Moused over!'));
+    x.addEventListener('mouseover', () => myFunction('Moused over!'));
     x.addEventListener('click', mySecondFunction('Clicked!'));
     x.addEventListener('mouseout', myThirdFunction('Moused out!'));
 }
@@ -10,9 +10,9 @@ function myFunction(msg) {
 }
 
 function mySecondFunction(msg) {
-    document.getElementById('demo').textContent = msg;
+    return () => document.getElementById('demo').textContent = msg;
 }
 
 function myThirdFunction(msg) {
-    document.getElementById('demo').textContent = msg;
+    return () => document.getElementById('demo').textContent = msg;
 }
