3391b46e1415ccc44877bf56e0b98225dd1e33fc
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2024-02-05 10:27:04 -0500

    modules

---
+++ scripts/math.js
@@ -0,0 +1,12 @@
+// mathOperations.js
+
+// Function to add two numbers
+export function add(a, b) {
+    return a + b;
+  }
+  
+// Function to multiply two numbers
+export function multiply(a, b) {
+return a * b;
+}
+  
