c96402d2c98b8dbf6a4a1ed2f0ef08784b4f5248
Duolikun-Daniya <daniya.d@northeastern.edu>
2024-02-06 12:41:45 -0500

    Complete stack

--- quiz/scripts/stack-q1.js
+++ quiz/scripts/stack-q1.js
@@ -8,7 +8,6 @@ class PStack {
   showId() {
     return this.#id;
   }
-
 }
 
 class PStackImpl extends PStack {
@@ -17,17 +16,29 @@ class PStackImpl extends PStack {
   }
 
   push(p) {
-    return this._persons.push(p)
+    return this._persons.push(p);
   }
 
   pop() {
-    return this._persons.pop().age
+    return this._persons.pop().age;
+  }
+
+  get persons() {
+    return this._persons;
+  }
+
+  set persons(person) {
+    this._persons = person;
   }
 }
 
 let pstack = new PStackImpl();
-pstack.persons = [{name: 'Jojo', age: 21}, {name: 'Gabi', age: 29}]
-pstack.push({name: 'Dein', age: 19});
-console.log(pstack.pop());
-console.log(pstack.pop());
+pstack.persons = [
+  { name: "Jojo", age: 21 },
+  { name: "Gabi", age: 29 },
+]; // Created an array with two objects in it
+// Now it is calling the setter method
+pstack.push({ name: "Dein", age: 19 }); // This actually created an person object
+console.log(pstack.pop()); // removed
+console.log(pstack.pop()); // the stack is empty now
 console.log(pstack.persons);
--- scripts/stack-q.js
+++ scripts/stack-q.js
@@ -1,13 +1,18 @@
+//node scripts/stack-q.js
 class PStack {
   #id;
   constructor() {
-    this.#id = 1;
+    this.#id = Math.floor(Math.random() * 100) + 1;
   }
 
   showId() {
+    console.log(this);
     return this.#id;
   }
 
+  getId() {
+    return this.#id;
+  }
 }
 
 class PStackImpl extends PStack {
@@ -16,23 +21,28 @@ class PStackImpl extends PStack {
     this.persons = [];
   }
   push(p) {
-    return this.persons.push(p)
+    return this.persons.push(p);
   }
 
   pop() {
-    return this.persons.pop().age
+    return this.persons.pop().age;
   }
 
   show() {
-    return this.persons
+    return this.persons;
   }
 
+  showId() {
+    console.log(this);
+    return this.getId;
+  }
 }
 
 let pstack = new PStackImpl();
-pstack.push({name: 'Jojo', age: 21});
-pstack.push({name: 'Gabi', age: 29});
-pstack.push({name: 'Dein', age: 19});
+pstack.push({ name: "Jojo", age: 21 });
+pstack.push({ name: "Gabi", age: 29 });
+pstack.push({ name: "Dein", age: 19 });
 console.log(pstack.pop());
 console.log(pstack.pop());
 console.log(pstack.show());
+console.log(pstack.showId());
