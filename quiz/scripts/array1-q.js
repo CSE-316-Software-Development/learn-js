610deff165f8f983986b457212bc5ca3955304d8
Harshit Sai Kannam <kannam.h@northeastern.edu>
2024-02-07 13:17:26 -0500

    array 1q

--- quiz/scripts/array1-q.js
+++ quiz/scripts/array1-q.js
@@ -8,25 +8,19 @@ window.onload = function() {
     const MAX_HISTORY = 5;
 
     btn.onclick = () => {
-    // we will only allow a term to be entered if the search input isn't empty
-      if (inp.value !== '') {
-              // empty the list so that we don't display duplicate entries
-              // the display is regenerated every time a search term is entered.
-              list.innerHTML = '';
-
-              // loop through the sorted array, and display all the search terms in the list
-              for (const itemText of myHistoryCopy) {
-                
-              }
-
-              // If the array length is 5 or more, remove the oldest search term
-              if (myHistory.length >= MAX_HISTORY) {
-                
-              }
-
-              // empty the search input and focus it, ready for the next term to be entered
-              inp.value = '';
-              btn.focus();
-          }
-    }
-}
+        if (inp.value !== '') {
+            myHistory.unshift(inp.value);
+            if (myHistory.length > MAX_HISTORY) {
+                myHistory.pop();
+            }
+            list.innerHTML = '';
+            myHistory.forEach((itemText) => {
+                const listItem = document.createElement('li');
+                listItem.textContent = itemText;
+                list.appendChild(listItem);
+            });
+            inp.value = '';
+            inp.focus();
+        }
+    };
+};
